dfa6904e0884215dfa06180eac84276a46c0cdb2
Phạm Minh Hoàng <64359566+protam113@users.noreply.github.com>
2025-01-20 09:55:40 +0700

    Update MissionProb.tsx

--- src/components/main/home/components/prob/MissionProb.tsx
+++ src/components/main/home/components/prob/MissionProb.tsx
@@ -23,7 +23,7 @@ const MissionProb: React.FC<SuvuCardProps> = ({
 }) => {
   return (
     <Link
-      href={`/hoi_dong/su_vu/${id}`}
+      href={`/mission/${id}`}
       className="rounded-lg shadow-lg overflow-hidden bg-white transform transition-transform duration-300 hover:scale-105  hover:text-yellow-500" // Thêm hiệu ứng hover
     >
       <LazyLoad
